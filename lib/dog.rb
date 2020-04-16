ccb00713effaca9486660e7827b3394343d46769
Elijah wines <elijahwines84@yahoo.com>
2020-04-16 14:52:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,9 +20,9 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def self.drop_table 
-    sql = <<-SQL 
-      DROP TABLE dogs 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
     SQL
 
     DB[:conn].execute(sql)
