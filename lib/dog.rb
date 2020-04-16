7e85c78687904335ab79431ab40bff3ec487f55e
Elijah wines <elijahwines84@yahoo.com>
2020-04-16 14:46:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  attr_accessor :id, :name, :breed 
+  attr_accessor :id, :name, :breed
 
   def initialize(name, breed, id = nil)
-    @name = name 
-    @breed = breed 
-    @id = id 
+    @name = name
+    @breed = breed
+    @id = id
   end
 
   
