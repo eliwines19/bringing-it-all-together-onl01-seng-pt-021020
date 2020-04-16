867002d81b3702b791aa8f3bbbdb65899382539d
Elijah wines <elijahwines84@yahoo.com>
2020-04-16 14:51:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,11 +9,11 @@ class Dog
   end
 
   def self.create_table
-    sql = <<-SQL 
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        age INTEGER 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER
       )
     SQL
 
