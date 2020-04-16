6b6c42f95b1ffbfaf21e4ec1a6fd87bfcaf9334d
Elijah wines <elijahwines84@yahoo.com>
2020-04-16 15:12:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -83,4 +83,12 @@ class Dog
        dog
    end
 
+   def update 
+     sql = <<-SQL
+      "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+     SQL
+
+     DB[:conn].execute(sql, self.name, self.breed, self.id)
+   end
+
 end
